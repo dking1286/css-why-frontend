f0a85f3fad1e1e237b774e6a97c9e13441e30b68
Daniel O King <daniel.oliver.king@gmail.com>
2025-01-24 15:11:35 -0800

    Add .ocamlformat file for frontend repository.

--- lib/root/Root.ml
+++ lib/root/Root.ml
@@ -2,4 +2,6 @@ open Webapi.Dom
 
 let showTextInRoot text =
   let root = document |> Document.getElementById "root" in
-  match root with None -> () | Some node -> Element.setInnerText node text
+  match root with
+  | None -> ()
+  | Some node -> Element.setInnerText node text
